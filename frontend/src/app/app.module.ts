dde609b15f87485215c778470481615b99350dbf
Audrie8a <ann.audrie8a@gmail.com>
2021-08-20 16:26:19 -0600

    Avance frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { AnalisisComponent } from './Componentes/analisis/analisis.component';
+import { ReportesComponent } from './Componentes/reportes/reportes.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DatosComponent } from './Componentes/datos/datos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AnalisisComponent,
+    ReportesComponent,
+    DatosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    MatTabsModule,
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
